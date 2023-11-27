05f2a0f65f67ebfe8fce126a0a39495446fc04ba
MohanaVR <145452282+MohanaVR@users.noreply.github.com>
2023-11-27 14:21:31 +0530

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -4,5 +4,11 @@
 sudo apt update
 sudo apt install -y apache2
 
+# Get the instance's public IP address
+public_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+# Print the IP address to a local file
+echo "Instance Public IP: $public_ip" > instance_ip.txt
+
 # Print a message indicating the installation is complete
 echo "Apache2 installation complete."
